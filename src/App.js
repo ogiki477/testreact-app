70d3f90cc2d21dddd81a5d4dd94ecbb5a5ee8989
Ogiki Moses Odera <omojick88moses@gmail.com>
2024-11-21 14:29:07 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Omo Moses 07777777
         </a>
       </header>
     </div>
